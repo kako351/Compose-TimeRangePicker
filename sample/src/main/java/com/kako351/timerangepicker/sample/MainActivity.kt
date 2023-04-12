c4814d02f4e9fc400b1a80c047b6ff1eea9765f0
kako351 <h3.birth@gmail.com>
2023-04-13 00:18:17 +0900

    use onChangedTimeRange

--- sample/src/main/java/com/kako351/timerangepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/kako351/timerangepicker/sample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kako351.timerangepicker.sample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,7 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TimeRangePicker()
+                    TimeRangePicker { startHour, startMinutes, endHour, endMinutes ->
+                        Log.i("MainActivity", "startHour: $startHour, startMinutes: $startMinutes, endHour: $endHour, endMinutes: $endMinutes")
+                    }
                 }
             }
         }
