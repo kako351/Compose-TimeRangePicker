3edd8e8b0f4ccc25df8fb1645e4efd10a59338d9
kako351 <h3.birth@gmail.com>
2023-07-09 16:05:27 +0900

    fix: delete dead code

--- sample/src/main/java/com/kako351/timerangepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/kako351/timerangepicker/sample/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import com.kako351.timerangepicker.TimeRangePicker
 import com.kako351.timerangepicker.sample.ui.theme.TimeRangePickerTheme
 
@@ -22,31 +16,13 @@ class MainActivity : ComponentActivity() {
             TimeRangePickerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-//                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     TimeRangePicker { startHour, startMinutes, endHour, endMinutes ->
                         Log.i("MainActivity", "startHour: $startHour, startMinutes: $startMinutes, endHour: $endHour, endMinutes: $endMinutes")
                     }
-                    Text(text = "Hello World!", style = TextStyle())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TimeRangePickerTheme {
-        Greeting("Android")
     }
 }
